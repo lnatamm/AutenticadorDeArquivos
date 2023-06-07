2019d5e9eca5794b0dbb9fceabf0602e877a16b2
Levi <lnatamm25@gmail.com>
2023-06-06 23:17:42 -0300

    Ajustando a interface para o trabalho

--- Programa/Main.java
+++ Programa/Main.java
@@ -20,22 +20,41 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int controle = 1;
         while(controle == 1) {
-            System.out.println("Qual arquivo deseja abrir?");
-            String nomeArquivo = sc.next();
-            File file = new File(System.getProperty("user.dir") + "\\Arquivo\\" + nomeArquivo + ".txt");
             Tree tree = new Tree();
+            System.out.println("Qual o primeiro arquivo que deseja abrir?");
+            String nomeArquivo = sc.next();
+            File original = new File(System.getProperty("user.dir") + "\\Arquivo\\" + nomeArquivo + ".txt");
+            System.out.println("Qual o segundo arquivo que deseja abrir?");
+            nomeArquivo = sc.next();
+            File testCase = new File(System.getProperty("user.dir") + "\\Arquivo\\" + nomeArquivo + ".txt");
             try {
-                if (!file.createNewFile()) {
-                    Scanner reader = new Scanner(file);
+                if (!(original.createNewFile() || testCase.createNewFile())) {
+                    Scanner reader = new Scanner(original);
                     while (reader.hasNextLine()){
                         tree.add(reader.nextLine());
                     }
-                    System.out.println(tree.getHashCode());
-                    System.out.println("Deseja abrir outro arquivo? 1-Sim 2-Não");
+                    String originalHash = tree.getHashCode();
+                    reader = new Scanner(testCase);
+                    tree.clear();
+                    while (reader.hasNextLine()){
+                        tree.add(reader.nextLine());
+                    }
+                    String testCaseHash = tree.getHashCode();
+                    if(originalHash.equals(testCaseHash)){
+                        System.out.println("O arquivo é original");
+                    }
+                    else {
+                        System.out.println("O arquivo é editado");
+                    }
+                    System.out.println("Deseja fazer outro caso de teste? 1-Sim 2-Não");
                     controle = Integer.parseInt(sc.next());
                 } else {
-                    System.out.println("Arquivo não existe");
+                    System.out.println(original.getName() + " não existe");
                     System.out.println("Criando arquivo...");
+                    if(testCase.createNewFile()){
+                        System.out.println(testCase.getName() + " não existe");
+                        System.out.println("Criando arquivo...");
+                    }
                 }
             } catch (IOException e) {
                 System.out.println("deu ruim");
